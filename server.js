73cb0655ddbc909074586213346b2b8d5ec1ed79
Rythmat <teotaniwha@gmail.com>
2025-03-03 14:27:10 -0700

    add cors

--- server.js
+++ server.js
@@ -5,8 +5,10 @@ const client = require('./db/client.js');
 client.connect();
 
 const express = require('express');
+const cors = require('cors');
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 
 //      GET REQUESTS      //
