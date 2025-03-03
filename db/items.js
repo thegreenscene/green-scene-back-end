d7c9a4eb81f29ff400f5206d6860796808994616
Rythmat <teotaniwha@gmail.com>
2025-03-03 12:14:20 -0700

    add item functions to api

--- db/items.js
+++ db/items.js
@@ -3,7 +3,7 @@ const client = require('./client.js');
 const createItem = async(
   itemName, itemQuantity, itemLocation, itemPrice, 
     itemImageUrl, itemDescription, itemType, itemSellerId
-) => {
+  ) => {
   try {
     await client.query(`
       INSERT INTO items (
@@ -19,4 +19,45 @@ const createItem = async(
   }
 }
 
-module.exports = createItem;
+const fetchItems = async() => {
+  try {
+    const {rows} = await client.query(`
+      SELECT * FROM items;
+    `);
+    return rows;
+  } catch (error) {
+    throw new Error('Bad Items Fetch')
+  }
+}
+
+const categoryFetch = async(categoryName) => {
+  try {
+    if(categoryName!='sprout'&&categoryName!='seed'&&categoryName!='tool'){
+      throw new Error('Invalid Item Category');
+    }
+    const {rows} = await client.query(`
+      SELECT * FROM items WHERE type=${categoryName};
+    `);
+    return rows;
+  } catch (error) {
+    throw new Error('Bad Category Fetch')
+  }
+}
+
+const oneItem = async(itemId) => {
+  try {
+    const {rows} = await client.query(`
+      SELECT * FROM items WHERE id=${itemId};
+    `);
+    return rows[0];
+  } catch (error) {
+    throw new Error('Bad Item Fetch');
+  }
+}
+
+module.exports = {
+  createItem,
+  fetchItems,
+  categoryFetch,
+  oneItem
+};
--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+const { createItem, fetchItems, categoryFetch, oneItem } = require('./db/items.js');
+
+
 const client = require('./db/client.js');
 client.connect();
 
@@ -5,14 +8,50 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
+
+//      GET REQUESTS      //
+//DEFAULT//
 app.get('/', (req, res) => {
   res.send('Welcome to The Green Scene!');
 })
-
+//ALL ITEMS//
 app.get('/api/items', async(req, res) => {
   try {
-    const allItems = await client.query(`SELECT * FROM items;`);
-    res.send(allItems.rows);
+    const allItems = await fetchItems();
+    if (allItems === undefined || allItems.length == 0) {
+      res.send('Nothing Here')
+    }else{
+      res.send(allItems);
+    }
+  } catch(err) {
+    res.send(err.message);
+  }
+});
+//ITEMS BY CATEGORY//
+app.get('/api/items/category/:category', async(req, res) => {
+  const { category } = req.params;
+  try {
+    const itemCategory = await categoryFetch(category);
+    if (itemCategory === undefined || itemCategory.length == 0) {
+      res.send('Nothing Here')
+    }else{
+      res.send(itemCategory);
+    }
+  } catch(err) {
+    res.send(err.message);
+  }
+});
+//ITEM BY ID//
+app.get('/api/items/:id', async(req, res) => {
+  const { id } = req.params;
+  try {
+    const item = await oneItem(id);
+    if(item === undefined){
+      res.send('Item not Found');
+    }else{
+      res.send(item);
+    }
+
   } catch(err) {
     res.send(err.message);
   }
