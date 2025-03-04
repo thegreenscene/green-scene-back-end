766e28f597d10bba8aa11fbbcf9440917a5ff51f
Daniel Almeida <almeida.danny@gmail.com>
2025-03-04 11:11:47 -0600

    add login functionality with token return and formatting

--- db/users.js
+++ db/users.js
@@ -5,24 +5,24 @@ const jwt = require('jsonwebtoken');
 const createUser = async (userName, password, location) => {
 
   const hashedPW = await bcrypt.hash(password, 10);
-  
+
   await client.query(`
     INSERT INTO users(username, password, location)
     VALUES ('${userName}', '${hashedPW}', '${location}');
     `);
 }
 
-const loginUser = async (inputUser, inputPassword) =>{
-  try{
-  const {rows} = await client.query(`
+const loginUser = async (inputUser, inputPassword) => {
+  try {
+    const { rows } = await client.query(`
     SELECT * FROM users WHERE username='${inputUser}';
     `);
     const userCheck = rows[0];
     if (userCheck) {
       const isPasswordValid = await bcrypt.compare(inputPassword, userCheck.password);
       if (isPasswordValid) {
-        const token = await jwt.sign({id: userCheck.id}, process.env.SECRET);
-        
+        const token = await jwt.sign({ id: userCheck.id }, process.env.SECRET);
+
         return token;
       }
     } else {
@@ -33,6 +33,6 @@ const loginUser = async (inputUser, inputPassword) =>{
   }
 }
 module.exports = {
-createUser,
-loginUser
+  createUser,
+  loginUser
 }
